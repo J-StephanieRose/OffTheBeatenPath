9634907b2fd19ea442383f26239796edfde4cd80
J. Stephanie Rose <jsr67@pitt.edu>
2018-01-28 04:31:19 -0500

    Update TagRepository.java

--- GeoTrot/Repositories/TagRepository.java
+++ GeoTrot/Repositories/TagRepository.java
@@ -1,1 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.infsci2560.repositories;
 
+import edu.infsci2560.models.tag;
+import org.springframework.data.repository.PagingAndSortingRepository;
+
+/**
+ *
+ * @author kolobj
+ */
+public interface TagRepository extends PagingAndSortingRepository<Tag, Long> {}
