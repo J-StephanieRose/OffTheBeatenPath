8ab5c09ef4ffb0d4982b65bad02552c14917b601
J. Stephanie Rose <jsr67@pitt.edu>
2018-01-27 20:34:11 -0500

    Create MvcConfig.java

---
+++ GeoTrot/MvcConfig.java
@@ -0,0 +1,18 @@
+package edu.infsci2560;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+public class MvcConfig extends WebMvcConfigurerAdapter {
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/home").setViewName("home");
+        registry.addViewController("/").setViewName("home");
+        registry.addViewController("/dvds").setViewName("dvds");
+        registry.addViewController("/login").setViewName("login");
+    }
+
+}
