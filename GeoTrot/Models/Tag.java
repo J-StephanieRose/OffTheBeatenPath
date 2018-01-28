d3fd80acf501b8b7c43ec4aae904a4da1c8f388a
J. Stephanie Rose <jsr67@pitt.edu>
2018-01-27 22:51:21 -0500

    Update Tag.java

--- GeoTrot/Models/Tag.java
+++ GeoTrot/Models/Tag.java
@@ -1,1 +1,106 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.infsci2560.models;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import org.apache.commons.lang3.builder.EqualsBuilder;
+import org.apache.commons.lang3.builder.HashCodeBuilder;
+
+/**
+ *
+ * @author kolobj
+ */
+@Entity
+public class Tag {
+
+    private static final long serialVersionUID = 1L;
+
+    public enum TagType {
+        TagModified,
+        Strength,
+        Cardio,
+        CrossTrain
+    }
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    protected Long id;
+    protected String title;
+    protected WorkoutType workoutType;
+
+    public Dvd() {
+        this.id = Long.MAX_VALUE;
+        this.title = null;
+        this.workoutType = WorkoutType.Unknown;
+    }
+
+    public Dvd(Long id, String name, WorkoutType workoutType) {
+        this.id = id;
+        this.title = name;
+        this.workoutType = workoutType;
+    }
+
+    @Override
+    public String toString() {
+        return "[ id=" + this.id + ", title=" + this.title + ", workoutType=" + this.workoutType + " ]";
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        return EqualsBuilder.reflectionEquals(this, other);
+    }
+
+    @Override
+    public int hashCode() {
+        return HashCodeBuilder.reflectionHashCode(this);
+    }
+
+    /**
+     * @return the name
+     */
+    public String getTitle() {
+        return title;
+    }
+
+    /**
+     * @param title the name to set
+     */
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    /**
+     * @return the workoutType
+     */
+    public WorkoutType getWorkoutType() {
+        return workoutType;
+    }
+
+    /**
+     * @param workoutType the workoutType to set
+     */
+    public void setWorkoutType(WorkoutType workoutType) {
+        this.workoutType = workoutType;
+    }
+
+    /**
+     * @return the id
+     */
+    public Long getId() {
+        return id;
+    }
+
+    /**
+     * @param id the id to set
+     */
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+}
